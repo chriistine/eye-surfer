37d8183e4b2f96c15340e034f125ee3fcd829c8e
andaniel01 <65532880+andaniel01@users.noreply.github.com>
2021-03-06 17:11:17 -0500

    webnavigation.py

---
+++ WebNavigation.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+
+
+def searchForNews(topic, driver):
+
+    searchBtn = driver.find_element_by_class_name("top-menu-search__button")
+    searchBtn.click()
+
+    inputElement = driver.find_element_by_css_selector('input[name=search_text]')
+
+    inputElement.send_keys(topic)
+    inputElement.send_keys(Keys.ENTER)
+
+    #return list of articles on the first page
+    articleHeaders = []
+    time.sleep(2)
+    articleHeaders = driver.find_elements_by_class_name("article-card__link")
+
+    for article in articleHeaders:
+        
+        print(article)
+
+
+
+
+#def selectNews()
+
+
+def main():
+    driver = webdriver.Chrome(r'C:\Users\danie\dev\deltahacks\chromedriver.exe')
+
+    driver.get('https://nationalpost.com/')
+    searchForNews("education", driver)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
