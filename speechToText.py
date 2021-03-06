73b7d7b4092beaf58cac9cd38af87eb514de95a7
clee <christineunlee@gmail.com>
2021-03-06 16:05:36 -0500

    Added NLP Api

--- speechToText.py
+++ speechToText.py
@@ -4,13 +4,13 @@ import os
 import io
 import pyaudio
 import wave
-from google.cloud import speech, vision
+from google.cloud import speech, language_v1
 
 
 """ Listen to input audio and save as recording """
 
 # Get credentials
-credential_path = "/your/path/to/api.json"
+credential_path = os.path.join(os.path.dirname(__file__), './','deltahacks-306803-01a16b6b0a26.json')
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 # Set up for Recording
@@ -18,7 +18,7 @@ chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 2
 fs = 44100  # Record at 44100 samples per second
-seconds = 3
+seconds = 5
 filename = "output.wav"
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
@@ -74,6 +74,40 @@ config = speech.RecognitionConfig(
 
 # Detects speech in the audio file
 response = client.recognize(config=config, audio=audio)
+text = "Error: Did not work"
 
 for result in response.results:
     print("Transcript: {}".format(result.alternatives[0].transcript))
+    text = result.alternatives[0].transcript
+
+# Instantiates a client
+langClient = language_v1.LanguageServiceClient()
+encoding_type = language_v1.EncodingType.UTF8
+
+# The text to analyze
+document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+# sentiment = langClient.analyze_sentiment(request={'document': document}).document_sentiment
+
+entities = langClient.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
+print(entities)
+
+for entity in entities.entities:
+    print('Representative name for the entity: {}'.format(entity.name))
+    print("Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+    print("Salience score: {}".format(entity.salience))
+
+    for metadata_name, metadata_value in entity.metadata.items():
+        print(u"{}: {}".format(metadata_name, metadata_value))
+
+    for mention in entity.mentions:
+        print(u"Mention text: {}".format(mention.text.content))
+
+        # Get the mention type, e.g. PROPER for proper noun
+        print(
+            u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
+        )
+
+# print("Text: {}".format(text))
+# print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
