016fe4a48e3ccf206d8b0ac712b69761a38b5319
Min Kim <minkim0320@gmail.com>
2021-03-06 22:43:43 -0500

    hohoho

--- main.py
+++ main.py
@@ -43,16 +43,16 @@ def callback(event):
 
 root.bind('<Return>', callback)
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas = tk.Canvas(root, height=700, width=700, bg="black")
 canvas.pack()
 
-frame = tk.Frame(root, bg="white")
-frame.place(relw=0.8, relheight=0.8, relx=0.1, rely=0.1)
+# frame = tk.Frame(root, bg="white")
+# frame.place(relw=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp)
-openFile.pack()
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42", command=runApps)
-runApps.pack()
+# openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp)
+# openFile.pack()
+# runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42", command=runApps)
+# runApps.pack()
 
 root.mainloop()
 
